e694e646f8a66d84df3567884d769f86a69a626b
bonyoji <vdt.vincent.ta@gmail.com>
2024-01-18 16:15:18 +0900

    Add method for player choice
    Lets player be codemaker or codebreaker

--- mastermind.rb
+++ mastermind.rb
@@ -7,26 +7,74 @@ class Game
     puts 'The Computer has chosen a secret code'
     puts 'Try to solve it within 12 turns!'
     puts "\r"
-    computer_code
-    codebreaker
+    @challenger = ''
+    playstyle
   end
 
-  def computer_code
-    @computer_code = Array.new(4) { rand(1..6) }
+  def playstyle
+    puts "\rDo you want to be the Code Maker? Y / N"
+    input = ''
+    until is_y_or_n(input)
+        input = gets.chomp.downcase
+        if !(is_y_or_n(input))
+          puts 'Please enter Y or N'
+        end
+    end
+    if input == 'y'
+      @challenger = 'cpu'
+      puts "\r"
+      codemaker
+    else
+      @challenger = 'human'
+      puts "\r"
+      @computer_code = random_code
+      codebreaker
+    end
+  end
+
+  def is_y_or_n(input)
+    ['y', 'n'].include? input
+  end
+
+  def codemaker
+    puts "Enter four numbers between 1 - 6"
+    user_input = gets.chomp
+    user_code = user_input.split('').map { |v| v.to_i}
+    if valid?(user_code)
+      cpu_challenge(user_code)
+    else
+      puts 'Not a valid code, try again.'
+      codemaker
+    end
+  end
+
+  def cpu_challenge(user_code)
+    turn = 1
+    cpu_guess = random_code
+    until turn > 12 || @@winner
+      if cpu_guess == user_code
+        @@winner = true
+      else
+        p cpu_guess
+      end
+    end
+  end
+
+  def random_code
+    Array.new(4) { rand(1..6) }
   end
 
   def codebreaker
     turn = 1
-    until turn > 12 || @@winner do
-      p @computer_code
+    until turn > 12 || @@winner
       puts 'Guess four numbers that are between 1 - 6'
       input = gets.chomp
-      @guess = input.split('').map{ |v| v.to_i}
+      user_guess = input.split('').map{ |v| v.to_i}
 
-      if valid?
-        check_guess
-        turn += 1
+      if valid?(user_guess)
         puts "\r"
+        check_guess(user_guess)
+        turn += 1
       else
         puts "\r"
         puts 'Not a valid entry!'
@@ -36,37 +84,44 @@ class Game
     game_over
   end
 
-  def valid?
-    if @guess.length == 4 && @guess.all? { |i| i.between?(1, 6) }
+  def valid?(array)
+    if array.length == 4 && array.all? { |i| i.between?(1, 6) }
       return true
     end
     return false
   end
 
-  def check_guess
-    if @guess == @computer_code
+  def check_guess(array)
+    if array == @computer_code
         @@winner = true
-        game_over
-    end
-    @hint = []
-    @guess.each_with_index do |v, i|
-      if v == @computer_code[i]
-        @hint[i] = 'G'
-      elsif @computer_code.include?(v)
-        @hint[i] = 'P' #Not sure how to stop duplicates for same number here
-      else
-        @hint[i] = nil
+    else
+      @hint = []
+      array.each_with_index do |v, i|
+        if v == @computer_code[i]
+          @hint[i] = 'G'
+        elsif @computer_code.include?(v)
+          @hint[i] = 'P' #Not sure how to stop duplicates for same number here
+        else
+          @hint[i] = nil
+        end
       end
+      p @hint
     end
   end
 
   def game_over
-    if @@winner
-      puts "\r"
-      puts 'You Win! Congratulations!'
+    if @challenger == 'human'
+      if @@winner
+        puts 'You Win! Congratulations!'
+      else
+        puts 'You Lose! Better luck next time!'
+      end
     else
-      puts "\r"
-      puts 'You Lose! Better luck next time!'
+      if @@winner
+        puts 'Computer Wins! You Lose!'
+      else
+        puts 'You Win! Congratulations!'
+      end
     end
   end
 end
