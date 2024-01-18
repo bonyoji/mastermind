3de3257eb620a8a5d718b502fcdc23320ea0ceb5
bonyoji <vdt.vincent.ta@gmail.com>
2024-01-18 15:31:07 +0900

    Add check_guess and game_over method
    Checks if the answer is correct, gives hint if not
    
    game_over checks for win or loss

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,5 @@
 class Game
-  @@turn = 1
+  @@winner = false
 
   def initialize
     puts "\r"
@@ -16,39 +16,57 @@ class Game
   end
 
   def codebreaker
-    puts 'Guess four numbers that are between 1 - 6'
-    input = gets.chomp
-    @guess = input.split('').map{ |v| v.to_i}
+    turn = 1
+    until turn > 12 || @@winner do
+      p @computer_code
+      puts 'Guess four numbers that are between 1 - 6'
+      input = gets.chomp
+      @guess = input.split('').map{ |v| v.to_i}
 
-    if valid?
+      if valid?
         check_guess
-    else
-      puts "\r"
-      puts 'Not a valid entry!'
-      codebreaker
+        turn += 1
+        puts "\r"
+      else
+        puts "\r"
+        puts 'Not a valid entry!'
+        codebreaker
+      end
     end
+    game_over
   end
 
   def valid?
-    if @guess.length == 4
-      @guess.each do |i|
-        if !(i.between?(1, 6))
-          return false
-        end
-      end
+    if @guess.length == 4 && @guess.all? { |i| i.between?(1, 6) }
       return true
     end
     return false
   end
 
   def check_guess
+    if @guess == @computer_code
+        @@winner = true
+        game_over
+    end
     @hint = []
     @guess.each_with_index do |v, i|
-      if v.to_i == @computer_code[i]
-        @hint << 'G'
-
-
+      if v == @computer_code[i]
+        @hint[i] = 'G'
+      elsif @computer_code.include?(v)
+        @hint[i] = 'P' #Not sure how to stop duplicates for same number here
+      else
+        @hint[i] = nil
       end
+    end
+  end
+
+  def game_over
+    if @@winner
+      puts "\r"
+      puts 'You Win! Congratulations!'
+    else
+      puts "\r"
+      puts 'You Lose! Better luck next time!'
     end
   end
 end
